c311384131cdff2f8ee1af4f86b97f22f105cf6e
Bartemix836 <114190309+Bartemix836@users.noreply.github.com>
2025-10-07 13:14:20 +0200

    Update test_crypto.py

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -116,35 +116,5 @@ def test_launch_browser(playwright_context, request):
     time.sleep(3)
 
 
-
-
-
-
-
-
-
-    # log_step("STEP 5: Clicked 'SPX'")
-    # base_page.click_element_by_selector('div.flexCell-RsFlttSS:has-text("SPX")')
-    # time.sleep(5)
-    #
-    # log_step("STEP 6: Clicked Metrics button")
-    # metrics_xpath = '/html/body/div[2]/div/div[6]/div/div[2]/div[1]/div[1]/div[2]/div[1]/div/div/div[1]'
-    # base_page.click_element_by_xpath(metrics_xpath)
-    # time.sleep(4)
-    #
-    # log_step("STEP 7: Clicked 'Technical' position")
-    # technical_xpath = '/html/body/div[7]/div[2]/span/div[1]/div/div/div[2]'
-    # base_page.click_element_by_xpath(technical_xpath)
-    # time.sleep(8)
-    #
-    # log_step('Assertion: check is visible: title="Technicals"')
-    # assert base_page.is_element_visible('.title-rweDqzH3')
-    # time.sleep(3)
-    #
-    # log_step("STEP 8: Change period (1 minute - 1 month)")
-    # for label in ["1m", "5m", "15m", "30m", "1h", "2h", "4h", "1D", "1W", "1M"]:
-    #     base_page.click_element_by_xpath(f'//*[@id="{label}"]')
-    #     log_step(f"STEP 8: Selected period: {label}")
-    #     time.sleep(3)
-
     log_step("Test case completed successfully - positive result.")
+
